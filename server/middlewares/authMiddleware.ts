738ca3e74611800c740d3e3c4c15752355c5fc8c
Siphamandla Mazibuko <s.mazibuko1108@gmail.com>
2025-11-20 14:02:44 +0200

    security: Authentication JWT secret improvement

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -16,7 +16,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   }
 
   const token = authHeader.split(" ")[1];
-  const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+  const JWT_SECRET = process.env.JWT_SECRET || "nexus_explosive_secret_biuonwfuwbi";
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
