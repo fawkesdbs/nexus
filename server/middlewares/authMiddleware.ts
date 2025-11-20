8fb65551d86fbeaf65e60835de9dc4ea023b0aee
Siphamandla Mazibuko <s.mazibuko1108@gmail.com>
2025-11-20 14:10:31 +0200

    security: Removed hardcoded secrets

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -16,7 +16,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   }
 
   const token = authHeader.split(" ")[1];
-  const JWT_SECRET = process.env.JWT_SECRET || "nexus_explosive_secret_biuonwfuwbi";
+  const JWT_SECRET = process.env.JWT_SECRET as string;
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
